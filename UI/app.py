25f50a81acc2bef732234069af7dcfbc88ef2877
Jakub Bures <56349494+KubaBury@users.noreply.github.com>
2021-12-10 10:34:55 +0100

    app adjustment

--- UI/app.py
+++ UI/app.py
@@ -63,116 +63,21 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
-def csv2dict():
-    file = open("../data/article_archive.csv", encoding='utf8')
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    zpravy_csv = []
-    for row in csvreader:
-        zpravy_csv.append(row)
-    file.close()
-
-    pom = zpravy_csv[0]
-
-    zpravy = [
-        {'id': 0, header[1]: pom[1], header[2]: pom[2], header[3]: pom[3], header[4]: pom[4], header[5]: pom[5],
-         header[6]: pom[6], header[7]: pom[7], header[8]: pom[8]}
-    ]
-
-    for i in range(1, np.size(zpravy_csv, 0)):
-        pom = zpravy_csv[i]
-        zpravy.append(
-            {'id': i, header[1]: pom[1], header[2]: pom[2], header[3]: pom[3], header[4]: pom[4], header[5]: pom[5],
-             header[6]: pom[6], header[7]: pom[7], header[8]: pom[8]})
-
-    return zpravy
-
-
-@cached(cache=TTLCache(maxsize=1024, ttl=1))
-def vyber_zpravy(pocet, kategorie):
-    zpravy = csv2dict()
-    articles_all = pd.DataFrame(zpravy)
-    articles_all.loc[articles_all['category'] == 'Zprávy z domova', 'category'] = 'Domov'
-    articles_all.loc[articles_all['category'] == 'Zprávy ze světa', 'category'] = 'Svět'
-    articles_all.loc[articles_all['category'] == 'Byznys zprávy', 'category'] = 'Ekonomika'
-    articles_all.loc[articles_all['category'] == 'Zajímavosti', 'category'] = 'Životní styl'
-    articles_all.loc[articles_all['category'] == 'Životní styl a společnost', 'category'] = 'Životní styl'
-    articles_all.loc[articles_all['category'].str.contains('Věda a technologie'), 'category'] = 'Věda'
-    articles_all.loc[articles_all['category'] == 'Česko', 'category'] = 'Domov'
-    articles_sort = articles_all.sort_values(by=['published'], ascending=False)
-
-    articles2 = []
-    velikost = 0
-    k = 0
-
-    while velikost <= pocet - 1:
-        if kategorie == 'all':
-            articles_kategorie = articles_sort
-        else:
-            articles_kategorie = articles_sort[articles_sort['category'] == kategorie]
-        articles = articles_kategorie.iloc[:pocet + k, [1, 3]]
-        vla = vectorized_lemmatized_articles(articles, 0, articles.shape[0])
-        ded = deduplicate(vla.run())
-        b = ded.run()
-        if len(b) != 0:
-            articles2 = articles.drop(articles.index[b])
-        else:
-            articles2 = articles
-
-        ind_pom = articles2.index
-        velikost = articles2.shape[0]
-        k = k + 1
-
-    zpravy_vybrane = articles_all.iloc[ind_pom].to_dict('records')
-    return zpravy_vybrane
-
-
-def personalizovane_zpravy(jmeno_uzivatele):
-    zpravy = csv2dict()
-    zpravy_all = pd.DataFrame(zpravy)
-    zpravy_all.loc[zpravy_all['category'] == 'Zprávy z domova', 'category'] = 'Domov'
-    zpravy_all.loc[zpravy_all['category'] == 'Zprávy ze světa', 'category'] = 'Svět'
-    zpravy_all.loc[zpravy_all['category'] == 'Byznys zprávy', 'category'] = 'Ekonomika'
-    zpravy_all.loc[zpravy_all['category'] == 'Zajímavosti', 'category'] = 'Životní styl'
-    zpravy_all.loc[zpravy_all['category'] == 'Životní styl a společnost', 'category'] = 'Životní styl'
-    zpravy_all.loc[zpravy_all['category'].str.contains('Věda a technologie'), 'category'] = 'Věda'
-    zpravy_all.loc[zpravy_all['category'] == 'Česko', 'category'] = 'Domov'
-    jmeno = jmeno_uzivatele
-    seznam = pd.read_csv("../data/likes.csv")
-    kliknute_indexy = seznam.loc[seznam['user'] == jmeno]['zprava_id'].to_numpy()
-    if len(kliknute_indexy) < 5:
-        return 'klikni alespoň na 5 článků!'
-    else:
-        vybrane_indexy_mod = persona(zpravy_all, kliknute_indexy)
-        vybrane_indexy = vybrane_indexy_mod.run()
-
-        zpravy_vybrane = zpravy_all.iloc[vybrane_indexy].to_dict('records')
-
-        return zpravy_vybrane
-
-
 @app.route('/', methods=['GET'])
-@cached(cache=TTLCache(maxsize=1024, ttl=1))
+@cached(cache=TTLCache(maxsize=1024, ttl=1000))
 def index():
     zpravy = vyber_zpravy(9, 'all')
     return render_template('index.html', zpravy=zpravy)
 
 
 @app.route('/<kategorie>', methods=['GET'])
-@cached(cache=TTLCache(maxsize=1024, ttl=1))
+@cached(cache=TTLCache(maxsize=1024, ttl=1000))
 def category(kategorie):
     zpravy = vyber_zpravy(9, kategorie=kategorie)
     return render_template('kategorie.html',
                            zpravy=zpravy,
                            page=kategorie
                            )
-
-
-@app.route('/<name>', methods=['GET'])
-@cached(cache=TTLCache(maxsize=1024, ttl=1))
-def personalized(name):
-    zpravy = personalizovane_zpravy(name=name)
-    return render_template('kategorie.html', zpravy=zpravy, page=name)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -224,6 +129,99 @@ def like(name, id):
     df = pd.DataFrame(data)
     df.to_csv('../data/likes.csv', index=False, header=False, mode='a')
     return redirect(url_for('index'))
+
+
+@app.route('/personalized/<name>', methods=['GET'])
+@cached(cache=TTLCache(maxsize=1024, ttl=1000))
+def personalized(name):
+    zpravy = personalizovane_zpravy(name)
+    return render_template('kategorie.html', zpravy=zpravy, page=name)
+
+
+def csv2dict():
+    file = open("../data/article_archive.csv", encoding='utf8')
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    zpravy_csv = []
+    for row in csvreader:
+        zpravy_csv.append(row)
+    file.close()
+
+    pom = zpravy_csv[0]
+
+    zpravy = [
+        {'id': 0, header[1]: pom[1], header[2]: pom[2], header[3]: pom[3], header[4]: pom[4], header[5]: pom[5],
+         header[6]: pom[6], header[7]: pom[7], header[8]: pom[8]}
+    ]
+
+    for i in range(1, np.size(zpravy_csv, 0)):
+        pom = zpravy_csv[i]
+        zpravy.append(
+            {'id': i, header[1]: pom[1], header[2]: pom[2], header[3]: pom[3], header[4]: pom[4], header[5]: pom[5],
+             header[6]: pom[6], header[7]: pom[7], header[8]: pom[8]})
+
+    return zpravy
+
+
+
+def vyber_zpravy(pocet, kategorie):
+    zpravy = csv2dict()
+    articles_all = pd.DataFrame(zpravy)
+    articles_all.loc[articles_all['category'] == 'Zprávy z domova', 'category'] = 'Domov'
+    articles_all.loc[articles_all['category'] == 'Zprávy ze světa', 'category'] = 'Svět'
+    articles_all.loc[articles_all['category'] == 'Byznys zprávy', 'category'] = 'Ekonomika'
+    articles_all.loc[articles_all['category'] == 'Zajímavosti', 'category'] = 'Životní styl'
+    articles_all.loc[articles_all['category'] == 'Životní styl a společnost', 'category'] = 'Životní styl'
+    articles_all.loc[articles_all['category'].str.contains('Věda a technologie'), 'category'] = 'Věda'
+    articles_all.loc[articles_all['category'] == 'Česko', 'category'] = 'Domov'
+    articles_sort = articles_all.sort_values(by=['published'], ascending=False)
+
+    velikost = 0
+    k = 0
+
+    while velikost <= pocet - 1:
+        if kategorie == 'all':
+            articles_kategorie = articles_sort
+        else:
+            articles_kategorie = articles_sort[articles_sort['category'] == kategorie]
+        articles = articles_kategorie.iloc[:pocet + k, [1, 3]]
+        vla = vectorized_lemmatized_articles(articles, 0, articles.shape[0])
+        ded = deduplicate(vla.run())
+        b = ded.run()
+        if len(b) != 0:
+            articles2 = articles.drop(articles.index[b])
+        else:
+            articles2 = articles
+
+        velikost = articles2.shape[0]
+        k = k + 1
+
+    zpravy_vybrane = articles_all.iloc[articles2.index].to_dict('records')
+    return zpravy_vybrane
+
+
+def personalizovane_zpravy(jmeno_uzivatele):
+    zpravy = csv2dict()
+    zpravy_all = pd.DataFrame(zpravy)
+    zpravy_all.loc[zpravy_all['category'] == 'Zprávy z domova', 'category'] = 'Domov'
+    zpravy_all.loc[zpravy_all['category'] == 'Zprávy ze světa', 'category'] = 'Svět'
+    zpravy_all.loc[zpravy_all['category'] == 'Byznys zprávy', 'category'] = 'Ekonomika'
+    zpravy_all.loc[zpravy_all['category'] == 'Zajímavosti', 'category'] = 'Životní styl'
+    zpravy_all.loc[zpravy_all['category'] == 'Životní styl a společnost', 'category'] = 'Životní styl'
+    zpravy_all.loc[zpravy_all['category'].str.contains('Věda a technologie'), 'category'] = 'Věda'
+    zpravy_all.loc[zpravy_all['category'] == 'Česko', 'category'] = 'Domov'
+    jmeno = jmeno_uzivatele
+    seznam = pd.read_csv("../data/likes.csv")
+    kliknute_indexy = seznam.loc[seznam['user'] == jmeno]['zprava_id'].to_numpy()
+    if len(kliknute_indexy) < 5:
+        return 'klikni alespoň na 5 článků!'
+    else:
+        vybrane_indexy_mod = persona(zpravy_all, kliknute_indexy)
+        vybrane_indexy = vybrane_indexy_mod.run()
+
+        zpravy_vybrane = zpravy_all.iloc[vybrane_indexy].to_dict('records')
+
+        return zpravy_vybrane
 
 
 if __name__ == "__main__":
